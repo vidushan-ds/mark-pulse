182b3ea0634ad8668fa83f1c8a1f5d704ffc8895
vidushan <vidushanpathir505@gmail.com>
2026-08-25 11:06:01 +0530

    feat: getting data from form

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return "Hello"
+    result = None
+    
+    if request.method == 'POST':
+        name = request.form.get('name', '')
+        mark = request.form.get('mark', type=float)
+        
+        result = {
+            "name" : name,
+            "mark" : mark
+        }
+    
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
