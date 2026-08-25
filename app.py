ca0512320d6a22409054cb4e2fbee41d77724f23
vidushan <vidushanpathir505@gmail.com>
2026-08-25 17:22:05 +0530

    feat: add css style, create grade_calculator

--- app.py
+++ app.py
@@ -2,6 +2,28 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def grade_calculator(marks: list):
+    
+    A_count = 0
+    B_count = 0
+    C_count = 0
+    S_count = 0
+    W_count = 0
+    
+    for mark in marks:
+        if mark >= 75:
+            A_count += 1
+        elif mark >= 65:
+            B_count += 1
+        elif mark >= 55:
+            C_count += 1
+        elif mark >= 35:
+            S_count += 1
+        else:
+            W_count += 1
+    
+    return [A_count, B_count, C_count, S_count, W_count]
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     result = None
@@ -18,6 +40,9 @@ def home():
         category_2 = request.form.get('category_2', type=float)
         category_3 = request.form.get('category_3', type=float)
         
+        marks = [science, mathematics, sinhala, english, history, religion, category_1, category_2, category_3]
+        grades = grade_calculator(marks)
+        
         result = {
             "exam_name" : exam_name,
             "science" : science,
@@ -28,7 +53,8 @@ def home():
             "religion" : religion,
             "category_1" : category_1,
             "category_2" : category_2,
-            "category_3" : category_3
+            "category_3" : category_3,
+            "grades" : grades
         }
     
     return render_template("index.html", result=result)
